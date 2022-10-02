5f54f1aa4084f363a50b1edbdc4edc7654c4f792
Ibnu Prayogi <ibnu.dev@gmail.com>
2022-10-02 15:07:39 +0700

    chore: fonts, colors, and deps

---
+++ src/constants/Colors.ts
@@ -0,0 +1,31 @@
+const basicColors = {
+  white: '#fff',
+  greyDarken: '#909EAB',
+  greyDarker: '#CBD0D7',
+  grey: '#F6F8FF',
+  black: '#14212A',
+  blackLighter: '#56606C',
+  blackBright: '#627381',
+  redTosca: '#ff5436',
+  redToscaLighter: '#ff8975',
+  greenTosca: '#50e3c2',
+  greenToscaLighter: '#73ffe0',
+  blueTosca: '#008fff',
+  blueToscaLighter: '#45adff',
+};
+
+export const LightColors = {
+  redTosca: basicColors.redTosca,
+  greenTosca: basicColors.greenTosca,
+  blueTosca: basicColors.blueTosca,
+  grey: basicColors.greyDarker,
+};
+
+export const DarkColors = {
+  redTosca: basicColors.redToscaLighter,
+  greenTosca: basicColors.greenToscaLighter,
+  blueTosca: basicColors.blueToscaLighter,
+  grey: basicColors.blackLighter,
+};
+
+export default basicColors;
